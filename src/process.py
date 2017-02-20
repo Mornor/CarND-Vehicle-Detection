2b90d63064338c30cd6c1e4893415bbcfe35e286
Mornor <cesliens@gmail.com>
2017-02-20 17:06:54 +0100

    First try on video

--- src/process.py
+++ src/process.py
@@ -14,11 +14,12 @@ from skimage.feature import hog
 from sklearn.svm import LinearSVC
 from sklearn.model_selection import train_test_split
 from scipy.ndimage.measurements import label
+from moviepy.editor import VideoFileClip
 import numpy as np
 import utils
 
 # Parameters
-color_space = 'YCrCb'			# Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+color_space = 'YCrCb'		# Can be RGB, HSV, LUV, HLS, YUV, YCrCb
 orient = 9  				# HOG orientations
 pix_per_cell = 8 			# HOG pixels per cell
 cell_per_block = 2 			# HOG cells per block
@@ -28,76 +29,112 @@ hist_bins = 16  			# Number of histogram bins
 spatial_feat = False 		# Spatial features on or off
 hist_feat = False 			# Histogram features on or off
 hog_feat = True 			# HOG features on or off
-y_start_stop = [380, 680]	# Min and max in y to search in slide_window()
+y_start_stop = [380, 550]	# Min and max in y to search in slide_window()
 x_start_stop = [800, 1280]	# Min and max in x to search in slide_window()
 
-# Load the images as RGB, 64x64
-car_images, not_car_images = utils.load_images(udacity=False)
+class Detector:
+	def __init__(self, svc, X_scaler):
+		self.svc = svc 
+		self.X_scaler = X_scaler
 
-# Extract useful feaures from images 
-car_images_features = utils.extract_features(car_images, color_space=color_space, 
-						spatial_size=spatial_size, hist_bins=hist_bins, 
-						orient=orient, pix_per_cell=pix_per_cell, 
-						cell_per_block=cell_per_block, 
-						hog_channel=hog_channel, spatial_feat=spatial_feat, 
-						hist_feat=hist_feat, hog_feat=hog_feat)
+	def process_frame(self, image):
+		# Test the result on one single image
+		draw_image = np.copy(image)
 
-not_car_images_features = utils.extract_features(not_car_images, color_space=color_space, 
-						spatial_size=spatial_size, hist_bins=hist_bins, 
-						orient=orient, pix_per_cell=pix_per_cell, 
-						cell_per_block=cell_per_block, 
-						hog_channel=hog_channel, spatial_feat=spatial_feat, 
-						hist_feat=hist_feat, hog_feat=hog_feat)
+		windows = utils.slide_window(image, x_start_stop=x_start_stop, y_start_stop=y_start_stop, xy_window=(96, 96), xy_overlap=(0.75, 0.75))
 
-# Normalize the features
-X, X_scaler, scaled_X = utils.normalize(car_images_features, not_car_images_features)
+		hot_windows = utils.search_windows(image, windows, self.svc, self.X_scaler, color_space=color_space, 
+								spatial_size=spatial_size, hist_bins=hist_bins, 
+								orient=orient, pix_per_cell=pix_per_cell, 
+								cell_per_block=cell_per_block, 
+								hog_channel=hog_channel, spatial_feat=spatial_feat, 
+								hist_feat=hist_feat, hog_feat=hog_feat)                       
 
-# Define the labels vector 
-y = np.hstack((np.ones(len(car_images_features)), np.zeros(len(not_car_images_features))))
+		window_img = utils.draw_boxes(draw_image, hot_windows, color=(0, 0, 255), thick=6)
 
-# Split up data into randomized training and test sets
-rand_state = np.random.randint(0, 100)
-X_train, X_test, y_train, y_test = train_test_split(scaled_X, y, test_size=0.2, random_state=rand_state)
+		# Find the place were is the most overlapping boxes by drawing a Heatmap
+		heat = np.zeros_like(window_img[:,:,0]).astype(np.float)
+		heat = utils.add_heat(heat, hot_windows)
+		heat = utils.apply_threshold(heat, 1)
+		heatmap = np.clip(heat, 0, 255)
+		labels = label(heatmap)
+		draw_img = utils.draw_labeled_bboxes(image, labels)
 
-# Train the SVC classifier
-print('Using:',orient,'orientations',pix_per_cell, 'pixels per cell and', cell_per_block,'cells per block')
-print('Feature vector length:', len(X_train[0]))
-svc = LinearSVC()
-svc.fit(X_train, y_train)
-print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
+		return draw_img
 
-# Test the result on one single image
-image = mpimg.imread('./test_images/test5.jpg')
-draw_image = np.copy(image)
 
-windows = utils.slide_window(image, x_start_stop=x_start_stop, y_start_stop=y_start_stop, xy_window=(96, 96), xy_overlap=(0.75, 0.75))
+def prepare_data():
+	# Load the images as RGB, 64x64
+	car_images, not_car_images = utils.load_images(udacity=False)
 
-hot_windows = utils.search_windows(image, windows, svc, X_scaler, color_space=color_space, 
-						spatial_size=spatial_size, hist_bins=hist_bins, 
-						orient=orient, pix_per_cell=pix_per_cell, 
-						cell_per_block=cell_per_block, 
-						hog_channel=hog_channel, spatial_feat=spatial_feat, 
-						hist_feat=hist_feat, hog_feat=hog_feat)                       
+	# Extract useful feaures from images 
+	car_images_features = utils.extract_features(car_images, color_space=color_space, 
+							spatial_size=spatial_size, hist_bins=hist_bins, 
+							orient=orient, pix_per_cell=pix_per_cell, 
+							cell_per_block=cell_per_block, 
+							hog_channel=hog_channel, spatial_feat=spatial_feat, 
+							hist_feat=hist_feat, hog_feat=hog_feat)
 
-window_img = utils.draw_boxes(draw_image, hot_windows, color=(0, 0, 255), thick=6)
+	not_car_images_features = utils.extract_features(not_car_images, color_space=color_space, 
+							spatial_size=spatial_size, hist_bins=hist_bins, 
+							orient=orient, pix_per_cell=pix_per_cell, 
+							cell_per_block=cell_per_block, 
+							hog_channel=hog_channel, spatial_feat=spatial_feat, 
+							hist_feat=hist_feat, hog_feat=hog_feat)
 
-# Find the place were is the most overlapping boxes by drawing a Heatmap
-heat = np.zeros_like(window_img[:,:,0]).astype(np.float)
-heat = utils.add_heat(heat, hot_windows)
-heat = utils.apply_threshold(heat, 1)
-heatmap = np.clip(heat, 0, 255)
-labels = label(heatmap)
-draw_img = utils.draw_labeled_bboxes(np.copy(window_img), labels)
+	# Normalize the features
+	X, X_scaler, scaled_X = utils.normalize(car_images_features, not_car_images_features)
 
-fig = plt.figure()
-plt.subplot(121)
-plt.imshow(draw_img)
-plt.title('Car Positions')
-plt.subplot(122)
-plt.imshow(heatmap, cmap='hot')
-plt.title('Heat Map')
-fig.tight_layout()
-plt.show()
+	# Define the labels vector 
+	y = np.hstack((np.ones(len(car_images_features)), np.zeros(len(not_car_images_features))))
+
+	# Split up data into randomized training and test sets
+	rand_state = np.random.randint(0, 100)
+	X_train, X_test, y_train, y_test = train_test_split(scaled_X, y, test_size=0.2, random_state=rand_state)
+
+	# Train the SVC classifier
+	#print('Using:',orient,'orientations',pix_per_cell, 'pixels per cell and', cell_per_block,'cells per block')
+	#print('Feature vector length:', len(X_train[0]))
+	svc = LinearSVC()
+	svc.fit(X_train, y_train)
+	
+	return svc, X_scaler
+	#print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
+
+def process(image, svc, X_scaler):
+	# Test the result on one single image
+	image = mpimg.imread(image)
+	draw_image = np.copy(image)
+
+	windows = utils.slide_window(image, x_start_stop=x_start_stop, y_start_stop=y_start_stop, xy_window=(96, 96), xy_overlap=(0.75, 0.75))
+
+	hot_windows = utils.search_windows(image, windows, svc, X_scaler, color_space=color_space, 
+							spatial_size=spatial_size, hist_bins=hist_bins, 
+							orient=orient, pix_per_cell=pix_per_cell, 
+							cell_per_block=cell_per_block, 
+							hog_channel=hog_channel, spatial_feat=spatial_feat, 
+							hist_feat=hist_feat, hog_feat=hog_feat)                       
+
+	window_img = utils.draw_boxes(draw_image, hot_windows, color=(0, 0, 255), thick=6)
+
+	# Find the place were is the most overlapping boxes by drawing a Heatmap
+	heat = np.zeros_like(window_img[:,:,0]).astype(np.float)
+	heat = utils.add_heat(heat, hot_windows)
+	heat = utils.apply_threshold(heat, 1)
+	heatmap = np.clip(heat, 0, 255)
+	labels = label(heatmap)
+	draw_img = utils.draw_labeled_bboxes(image, labels)
+
+	return draw_img
+
+
+svc, X_scaler = prepare_data()
+detector = Detector(svc, X_scaler)
+
+output = 'output.mp4'
+clip1 = VideoFileClip("project_video.mp4")
+white_clip = clip1.fl_image(detector.process_frame) #NOTE: this function expects color images!!
+white_clip.write_videofile(output, audio=False)
 
 #plt.imshow(window_img)
 #plt.show()
