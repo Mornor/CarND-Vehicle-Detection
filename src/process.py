9cf0e8858265598a90c61a8928dc031205ad03b1
Celien Nanson <cesliens@gmail.com>
2017-03-05 13:57:22 +0100

    Code refactoring

--- src/process.py
+++ src/process.py
@@ -20,15 +20,15 @@ import utils
 
 # Parameters
 color_space = 'YUV'			# Can be RGB, HSV, LUV, HLS, YUV, YCrCb
-orient = 9  				# HOG orientations
-pix_per_cell = 8 			# HOG pixels per cell
-cell_per_block = 2 			# HOG cells per block
-hog_channel = "ALL" 		# Can be 0, 1, 2, or "ALL"
-spatial_size = (32, 32) 	# Spatial binning dimensions
-hist_bins = 32  			# Number of histogram bins
-spatial_feat = True 		# Spatial features on or off
-hist_feat = True 			# Histogram features on or off
-hog_feat = True 			# HOG features on or off
+orient = 9					# HOG orientations
+pix_per_cell = 8			# HOG pixels per cell
+cell_per_block = 2			# HOG cells per block
+hog_channel = "ALL"			# Can be 0, 1, 2, or "ALL"
+spatial_size = (32, 32)		# Spatial binning dimensions
+hist_bins = 32				# Number of histogram bins
+spatial_feat = True			# Spatial features on or off
+hist_feat = True			# Histogram features on or off
+hog_feat = True				# HOG features on or off
 y_start_stop = [380, 550]	# Min and max in y to search in slide_window()
 x_start_stop = [730, 1280]	# Min and max in x to search in slide_window()
 
